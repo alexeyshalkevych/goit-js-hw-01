ccae9ab86d0bdef0968f4f3bddc8a3b6d4c31f12
Alexey <valnistuy@gmail.com>
2019-11-28 19:03:08 +0200

    change task-06

--- js/task-06.js
+++ js/task-06.js
@@ -22,7 +22,7 @@ let total = 0;
 do {
   input = prompt("Введите число");
 
-  if (isNaN(input)) {
+  if (Number.isNaN(input)) {
     alert("Было введено не число, попробуйте еще раз");
   } else {
     total += Number(input);
